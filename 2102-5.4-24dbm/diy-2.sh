66d4569dd931b9b13d5526e42fbcefc352c0e769
ffei1 <121517219@qq.com>
2026-01-17 21:34:58 +0800

    update

--- 2102-5.4-24dbm/diy-2.sh
+++ 2102-5.4-24dbm/diy-2.sh
@@ -14,5 +14,4 @@
 sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
 cp -f $GITHUB_WORKSPACE/2102-5.4-24dbm/21_ethinfo.js package/emortal/autocore/files/generic/21_ethinfo.js
 cp -f $GITHUB_WORKSPACE/2102-5.4-24dbm/02_network target/linux/mediatek/mt7981/base-files/etc/board.d/02_network
-rm package/mtk/drivers/mt_wifi/patches-7673/001-eeprom-flash-api.patch
 
